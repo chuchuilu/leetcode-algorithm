16905b5d9251d4a330e2611fee80a551c1ad3b22
hongyu.zhou <hongyu.zhou@momenta.ai>
2024-08-14 13:19:14 +0800

    feat: Add sliding window algorithm implementation

---
+++ 数组/滑动窗口最大值.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+#include <deque>
+using namespace std;
+
+
+
+vector<int> maxSlidingWindows(vector<int>& nums. int k){
+    vector<int>ans;
+    deque<int>dq;
+    int n = nums.size();
+    for(int i = 0; i < n; i++){
+        while(!dq.empty() && nums[i] >= nums[dq.back()]){
+            dq.pop_back();
+        }
+        dq.pop_back(i);
+        while(i - dq.front() >= k)  dq.pop_front();
+        if(i >= k - 1){
+            ans.push_back(nums[dq.front()]);
+        }
+        
+    }
+}
+
+
+
+int main(){
+
+
+
+
+
+    return 0;
+}
