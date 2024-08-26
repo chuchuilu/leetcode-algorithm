6d7586ff49a924add09393cbb0f3184494dbe548
hongyu.zhou <hongyu.zhou@momenta.ai>
2024-08-26 17:04:56 +0800

    Refactor code to implement big number division algorithm

--- 面试题/大数乘法.cpp
+++ 面试题/大数乘法.cpp
@@ -30,6 +30,35 @@ string multiply(string num1, string num2){
     return product.empty() ? "0" : product;
 }
 
+// 大数除法
+string divide(string num1, string num2){
+    if(num1 == "0") return "0";
+    if(num2 == "0") return "error";
+
+    int n1 = num1.size();
+    int n2 = num2.size();
+    vector<int> res(n1, 0);
+
+    for(int i = 0; i < n1; i++){
+        int j = 0;
+        while(j < n2){
+            if(num1[i] == num2[j]){
+                res[i] = 1;
+                break;
+            }
+            j++;
+        }
+    }
+
+    string product;
+    for(int num: res){
+        if(!(product.empty() && num == 0)){
+            product.push_back(num + '0');
+        }
+    }
+    return product.empty() ? "0" : product;
+}
+
 
 
 
